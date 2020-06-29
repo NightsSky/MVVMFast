3a1c11c32a6ac34bce850820af7eb5bcd87145b7
whoami <xiekanglin1992@outlook.com>
2020-06-29 14:33:13 +0800

    update lib

--- app/src/main/java/mvvm/whoami/mvvmfast/extension/KtExtension.kt
+++ app/src/main/java/mvvm/whoami/mvvmfast/extension/KtExtension.kt
@@ -6,17 +6,6 @@ import android.view.View
 /**
  * Created by XKL on 2018/8/28.
  */
-//val options = RequestOptions()
-////        .placeholder(R)	//加载成功之前占位图
-////        .error(R.mipmap.ic_banana)	//加载错误之后的错误图
-//        .diskCacheStrategy(DiskCacheStrategy.RESOURCE)	//只缓存最终的图片
-//
-//fun ImageView.load(url:Any) {
-//    Glide.with(this.context)
-//            .load(url)
-//            .apply(options)
-//            .into(this)
-//}
 
 fun View.gone() {
     this.visibility = View.GONE
